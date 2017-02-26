9001567ea9289ca34d29984e300a548bf4b09e60
Mike Meyer <github.com@meyer.fm>
2017-02-25 18:50:40 -0800

    updates for ESLint 2

---
+++ README.md
@@ -0,0 +1,11 @@
+# eslint-config-meyer
+
+This here is my ESLint config. It’s standard with a few stronger opinions. Don’t use this, use standard.
+
+Install it like so...
+
+`yarn add -D eslint-config-meyer eslint-plugin-react eslint-plugin-json`
+
+...or like so...
+
+`npm install --save-dev eslint-config-meyer eslint-plugin-react eslint-plugin-json`
---
+++ test.js
@@ -0,0 +1,29 @@
+var hello = "wow";
+
+const butts = {
+  'butts': hello,
+}
+
+let ok = () => null
+
+function wow () {
+    return <div />
+}
+
+switch(ok){
+case 2:
+
+  break;
+default:
+
+}
+
+const wow = {ok: ok, 'ok1': ok}
+
+console.log()
+console.info()
+console.warn()
+console.error()
+console.table()
+console.group('neat')
+console.groupEnd()
